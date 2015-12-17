7f80a40ed91989127f3ad1fbda5e0b96f0258598
Vladislav Markov <vladislav.u.markov@gmail.com>
2015-12-17 23:55:41 +0200

    new API adaptation

--- graphix-editor/src/graphix_editor.cpp
+++ graphix-editor/src/graphix_editor.cpp
@@ -1,14 +1,15 @@
 #include <gfx/graphix.h>
+#include <iostream>
 
 int main(){
     auto window = gfx::window::create("Graphix Editor");
 
     window->set_key_reaction(
-        [&window](gfx::key::code key, gfx::key::state state){
+        [](gfx::window &wnd, gfx::key::code key, gfx::key::state state){
         if (key == gfx::key::code::escape &&
             state == gfx::key::state::release
         ){
-            window->close();
+            wnd.close();
         }
     });
 
