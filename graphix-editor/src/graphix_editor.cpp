0972ee5462b835a1874e44feced45fc42f371123
Vladislav Markov <vladislav.u.markov>
2015-12-17 02:16:21 +0200

    key reactions

--- graphix-editor/src/graphix_editor.cpp
+++ graphix-editor/src/graphix_editor.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
 #include <gfx/graphix.h>
 
 int main(){
     auto window = gfx::window::create("Graphix Editor");
+
+    window->set_key_reaction(
+        [&window](gfx::key::code key, gfx::key::state state){
+        if (key == gfx::key::code::escape &&
+            state == gfx::key::state::release
+        ){
+            window->close();
+        }
+    });
 
     return gfx::run(*window);
 }
