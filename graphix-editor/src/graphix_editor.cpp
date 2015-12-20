8a0d07609aba15112e315e98d390c0be94b6aee5
Vladislav Markov <vladislav.u.markov>
2015-12-21 00:43:34 +0200

    scene, camera and all the settings

--- graphix-editor/src/graphix_editor.cpp
+++ graphix-editor/src/graphix_editor.cpp
@@ -3,6 +3,19 @@
 int main(){
     auto window = gfx::window::create("Graphix Editor");
 
+    auto camera = gfx::camera::create(
+        glm::vec3(0.0f, 0.0f,-3.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f)
+    );
+
+    auto scene = gfx::scene::create(
+        90.0f, window->get_width(), window->get_height(),
+        0.1f, 100.0f, camera.get(), glm::vec4(0.6f, 0.6f, 0.6f, 1.0f)
+    );
+
+    window->add(scene.get());
+
     window->set_key_reaction(
         [](gfx::window &wnd, gfx::key::code key, gfx::key::state state){
         if (key == gfx::key::code::escape &&
